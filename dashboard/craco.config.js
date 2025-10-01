b633180ee404084ea4a49c517c94ebc2337cf3a6
Siegfried Kiermayer <siegfried.kiermayer@sap.com>
2025-10-01 16:25:27 +0200

    vibe: fix fe issues after upgrade

--- dashboard/craco.config.js
+++ dashboard/craco.config.js
@@ -26,4 +26,16 @@ module.exports = {
       ignoreWarnings: [/Failed to parse source map/], // ignore source map warnings
     },
   },
+  jest: {
+    configure: {
+      setupFiles: ["<rootDir>/src/jest-setup.js"],
+      moduleNameMapper: {
+        "^cheerio/lib/utils$": "<rootDir>/src/__mocks__/cheerio-utils.js",
+        "^cheerio/lib/(.*)$": "<rootDir>/src/__mocks__/cheerio-$1.js",
+      },
+      transformIgnorePatterns: [
+        "node_modules/(?!(cheerio|axios|@bufbuild|@cds|@clr|@connectrpc|@lit|@lit-labs|lit|lit-html|lit-element|bail|ccount|cds|character-entities|comma-separated-tokens|decode-named-character-reference|escape-string-regexp|hast-util-whitespace|is-plain-obj|lodash-es|markdown-table|mdast-util-definitions|mdast-util-find-and-replace|mdast-util-from-markdown|mdast-util-gfm-autolink-literal|mdast-util-gfm|mdast-util-newline-to-break|mdast-util-to-hast|mdast-util-to-markdown|mdast-util-to-string|micromark-core-commonmark|micromark|monaco-editor|parse-entities|property-information|ramda|react-markdown|react-monaco-editor|react-syntax-highlighter|remark-breaks|remark-gfm|remark-parse|remark-rehype|space-separated-tokens|swagger-client|swagger-ui-react|trim-lines|trough|unified|unist-builder|unist-util-generated|unist-util-is|unist-util-position|unist-util-stringify-position|unist-util-visit-parents|unist-util-visit|util-find-and-replace|vfile-message|vfile)/)",
+      ],
+    },
+  },
 };
---
+++ dashboard/src/jest-setup.js
@@ -0,0 +1,159 @@
+// Copyright 2018-2023 the Kubeapps contributors.
+// SPDX-License-Identifier: Apache-2.0
+
+// This file runs before any modules are imported during Jest test execution
+// It provides essential global polyfills that are required by modern packages like undici
+
+const { TextEncoder, TextDecoder } = require('util');
+const { EventEmitter } = require('events');
+
+// Make TextEncoder and TextDecoder available globally
+// This is required by undici and other modern packages that expect these to be available
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+// MessagePort and MessageChannel polyfills
+if (typeof global.MessagePort === 'undefined') {
+  global.MessagePort = class MessagePort extends EventEmitter {
+    constructor() {
+      super();
+      this.onmessage = null;
+      this.onmessageerror = null;
+    }
+
+    postMessage(data) {
+      // Simple implementation for testing
+      if (this.onmessage) {
+        this.onmessage({ data });
+      }
+    }
+
+    start() {}
+    close() {}
+  };
+}
+
+if (typeof global.MessageChannel === 'undefined') {
+  global.MessageChannel = class MessageChannel {
+    constructor() {
+      this.port1 = new global.MessagePort();
+      this.port2 = new global.MessagePort();
+    }
+  };
+}
+
+// ReadableStream polyfill for undici
+if (typeof global.ReadableStream === 'undefined') {
+  global.ReadableStream = class ReadableStream {
+    constructor(source) {
+      this.source = source;
+    }
+
+    getReader() {
+      return {
+        read: () => Promise.resolve({ done: true, value: undefined }),
+        releaseLock: () => {},
+        closed: Promise.resolve()
+      };
+    }
+
+    cancel() {
+      return Promise.resolve();
+    }
+
+    pipeTo(dest) {
+      return Promise.resolve();
+    }
+  };
+}
+
+// WritableStream polyfill
+if (typeof global.WritableStream === 'undefined') {
+  global.WritableStream = class WritableStream {
+    constructor() {}
+
+    getWriter() {
+      return {
+        write: () => Promise.resolve(),
+        close: () => Promise.resolve(),
+        abort: () => Promise.resolve(),
+        releaseLock: () => {}
+      };
+    }
+  };
+}
+
+// TransformStream polyfill
+if (typeof global.TransformStream === 'undefined') {
+  global.TransformStream = class TransformStream {
+    constructor() {
+      this.readable = new global.ReadableStream();
+      this.writable = new global.WritableStream();
+    }
+  };
+}
+
+// File and Blob polyfills
+if (typeof global.File === 'undefined') {
+  global.File = class File {
+    constructor(chunks, filename, options = {}) {
+      this.name = filename;
+      this.size = 0;
+      this.type = options.type || '';
+      this.lastModified = Date.now();
+    }
+  };
+}
+
+if (typeof global.Blob === 'undefined') {
+  global.Blob = class Blob {
+    constructor(chunks = [], options = {}) {
+      this.size = 0;
+      this.type = options.type || '';
+    }
+
+    slice() {
+      return new Blob();
+    }
+
+    text() {
+      return Promise.resolve('');
+    }
+
+    arrayBuffer() {
+      return Promise.resolve(new ArrayBuffer(0));
+    }
+  };
+}
+
+// FormData polyfill
+if (typeof global.FormData === 'undefined') {
+  global.FormData = class FormData {
+    constructor() {
+      this.data = new Map();
+    }
+
+    append(key, value) {
+      this.data.set(key, value);
+    }
+
+    get(key) {
+      return this.data.get(key);
+    }
+
+    has(key) {
+      return this.data.has(key);
+    }
+  };
+}
+
+// Also provide fetch-related globals that might be needed
+if (typeof global.fetch === 'undefined') {
+  // These will be available if needed, but we don't want to override existing implementations
+  global.fetch = jest.fn();
+  global.Request = jest.fn();
+  global.Response = jest.fn();
+  global.Headers = jest.fn();
+}
+
+
