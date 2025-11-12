ee7f7b7b7c8ed8aeb25af51437b98e4351bcbd66
Siegfried Kiermayer <siegfried.kiermayer@sap.com>
2025-11-12 14:28:27 +0100

    update social-card with kubeapps

--- site/docusaurus.config.ts
+++ site/docusaurus.config.ts
@@ -146,7 +146,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/kubeapps-social-card.png',
     colorMode: {
       respectPrefersColorScheme: true,
     },
